826f679a55b053acb6d5debde0329f5aa486fc9d
Californium251 <californium251@gmail.com>
2017-01-23 23:14:35 +0300

    Перепсал функции (как мне кажется) по-человечески

--- js/stat.js
+++ js/stat.js
@@ -3,6 +3,8 @@ window.renderStatistics = function(ctx, names, times) {
   var COL_WIDTH = 40;
   var SPACE_BETWEEN = 50;
   var FIRST_COL_POSITION = 150;
+  var COLUMN_SPACING_TOP = -10;
+  var COLUMN_SPACING_BOTTOM = 20;
   var BUBBLE_POS_X = 100;
   var BUBBLE_POS_Y = 10;
   var BUBBLE_WIDTH = 420;
@@ -15,6 +17,7 @@ window.renderStatistics = function(ctx, names, times) {
   var BUBBLE_PADDING_X = 20;
   var BUBBLE_PADDING_Y = 40;
   var INTERLINE_SPACING = 20;
+  var CHART_Y = 110;
   function createBubble (x, y, width, height, color, shadowColor) {
     ctx.fillStyle = shadowColor;
     ctx.fillRect(x + 10, y + 10, width, height);
@@ -27,7 +30,7 @@ window.renderStatistics = function(ctx, names, times) {
     var arr = textValue.split('\n');
     for (var i = 0; i < arr.length; i++) {
       ctx.fillText(arr[i], x, y + i * INTERLINE_SPACING);
-    }
+    };
   };
   function newColumn(name, result, colNumber) {
     var setScale = function(chartHeight) {
@@ -50,10 +53,10 @@ window.renderStatistics = function(ctx, names, times) {
     };
     var columnHeight = setScale(CHART_HEIGHT) * result;
     ctx.fillStyle = setColor(name);
-    ctx.fillRect(setX(colNumber), 230 - columnHeight, 50, columnHeight);
+    ctx.fillRect(setX(colNumber), CHART_Y + CHART_HEIGHT - columnHeight, COL_WIDTH, columnHeight);
     ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillText(Math.floor(result), setX(colNumber), 220 - columnHeight);
-    ctx.fillText(name, setX(colNumber),250);
+    ctx.fillText(Math.floor(result), setX(colNumber), CHART_Y + CHART_HEIGHT + COLUMN_SPACING_TOP - columnHeight);
+    ctx.fillText(name, setX(colNumber),CHART_Y + CHART_HEIGHT + COLUMN_SPACING_BOTTOM);
   };
   createBubble(BUBBLE_POS_X, BUBBLE_POS_Y, BUBBLE_WIDTH, BUBBLE_HEIGHT, BUBBLE_COLOR, BUBBLE_SHADOW_COLOR);
   typeText(WELCOME_TEXT, TEXT_STYLE, TEXT_COLOR, BUBBLE_POS_X + BUBBLE_PADDING_X, BUBBLE_POS_Y + BUBBLE_PADDING_Y);

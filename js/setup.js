5a07379e25b8a978c18884e0ec453e912e36feb1
Californium251 <californium251@gmail.com>
2017-02-04 20:10:18 +0300

    Добавил клавиатурные события, роли, tabindex'ы

--- js/setup.js
+++ js/setup.js
@@ -4,11 +4,15 @@
 
 'use strict';
 
+var ENTER_KEY_CODE = 13;
+var ESC_KEY_CODE = 27;
+
 var setup = {
   setupWindow: document.querySelector('.setup'),
   openButton: document.querySelector('.setup-open'),
   closeButton: document.querySelector('.setup-close'),
-  nameField: document.querySelector('.setup-user-name')
+  nameField: document.querySelector('.setup-user-name'),
+  saveButton: document.querySelector('.setup-submit')
 };
 
 var wizard = {
@@ -37,24 +41,86 @@ function setNewColor(colorSet) {
 
 function openOrClose(windowToOperate, flag, hideClassName) {
   windowToOperate.classList.toggle(hideClassName, flag);
+  function closeOnEsc(evt) {
+    if (checkTheKey(evt.keyCode, ESC_KEY_CODE)) {
+      openOrClose(windowToOperate, true, hideClassName);
+    }
+  }
+  if (flag) {
+    document.removeEventListener('keydown', closeOnEsc);
+  } else {
+    document.addEventListener('keydown', closeOnEsc);
+  }
+
+}
+
+function checkTheKey(evtKeyDown, btnKeyCode) {
+  if (evtKeyDown && evtKeyDown === btnKeyCode) {
+    return true;
+  }
+  return false;
 }
 
 setup.openButton.addEventListener('click', function() {
   openOrClose(setup.setupWindow, false, INVISIBLE_CLASS);
 });
 
+setup.openButton.addEventListener('keydown', function(evt) {
+  if (checkTheKey(evt.keyCode, ENTER_KEY_CODE)) {
+    openOrClose(setup.setupWindow, false, INVISIBLE_CLASS);
+  }
+});
+
 setup.closeButton.addEventListener('click', function() {
   openOrClose(setup.setupWindow, true, INVISIBLE_CLASS);
+});
+
+setup.closeButton.addEventListener('keydown', function(evt) {
+  if (checkTheKey(evt.keyCode, ENTER_KEY_CODE)) {
+    openOrClose(setup.setupWindow, true, INVISIBLE_CLASS);
+  }
 });
 
 wizard.cloak.htmlNode.addEventListener('click', function() {
   this.style.fill = setNewColor(wizard.cloak.color);
 });
 
+wizard.cloak.htmlNode.addEventListener('keydown', function(evt) {
+  if (checkTheKey(evt.keyCode, ENTER_KEY_CODE)) {
+    this.style.fill = setNewColor(wizard.cloak.color);
+  }
+});
+
 wizard.eyes.htmlNode.addEventListener('click', function() {
   this.style.fill = setNewColor(wizard.eyes.color);
 });
 
+wizard.eyes.htmlNode.addEventListener('keydown', function(evt) {
+  if (checkTheKey(evt.keyCode, ENTER_KEY_CODE)) {
+    this.style.fill = setNewColor(wizard.eyes.color);
+  }
+});
+
 wizard.fireball.htmlNode.addEventListener('click', function() {
   wizard.fireball.htmlNode.style.backgroundColor = setNewColor(wizard.fireball.color);
+});
+
+wizard.fireball.htmlNode.addEventListener('keydown', function(evt) {
+  if (checkTheKey(evt.keyCode, ENTER_KEY_CODE)) {
+    wizard.fireball.htmlNode.style.backgroundColor = setNewColor(wizard.fireball.color);
+  }
+});
+
+
+//Я дописал код, который расположен ниже, НО кнопка Сохранить - это submit формы, так зачем добавлять еще обработчики,
+//Если все равно submit приводит к обновлению страницы? Я понимаю, что потом будет JSONP, но сейчас даже проверять неудобно
+
+setup.saveButton.addEventListener('click', function() {
+  openOrClose(setup.setupWindow, true, INVISIBLE_CLASS);
+});
+
+setup.saveButton.addEventListener('keydown', function(evt) {
+  if (checkTheKey(evt.keyCode, ENTER_KEY_CODE)) {
+    openOrClose(setup.setupWindow, true, INVISIBLE_CLASS);
+  }
 });
